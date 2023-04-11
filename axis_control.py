a89554d1004fa9bb400d996da4ec0fbddcfa5d8d
Robo_NK47 <kahanobeats@gmail.com>
2023-04-11 18:20:45 +0300

    Initial commit

--- axis_control.py
+++ axis_control.py
@@ -81,10 +81,10 @@ class Motor:
         GPIO.output(self.direction_pin, self.set_direction(direction))
         for step in tqdm(range(distance)):
             self.update_edge_sensors()
-            # if (self.direction_key[direction] and not self.edge_sensor_i_pin_state) or (
-            #         not self.direction_key[direction] and not self.edge_sensor_f_pin_state):
-            self.motor_single_step(velocity)
-            self.update_position(direction)
+            if (not self.direction_key[direction] and self.edge_sensor_i_pin_state) or (
+                self.direction_key[direction] and self.edge_sensor_f_pin_state):
+                self.motor_single_step(velocity)
+                self.update_position(direction)
             # print(self.edge_sensor_i_pin_state, self.edge_sensor_f_pin_state)
 
     def motor_while_loop(self, velocity, direction):
@@ -97,10 +97,33 @@ class Motor:
             self.motor_single_step(velocity)
             self.update_position(direction)
 
+    def axis_to_position(self, velocity, direction):
+        velocity = 1 / velocity
+        GPIO.output(self.direction_pin, self.set_direction(direction))
+        while (not self.direction_key[direction] and self.edge_sensor_i_pin_state) or (
+                self.direction_key[direction] and self.edge_sensor_f_pin_state):
+            self.update_edge_sensors()
+            self.motor_single_step(velocity)
+            self.update_position(direction)
+
+
+class PenAxis(Motor):
+    def __init__(self, name, step_pin, direction_pin, edge_sensor_i_pin, edge_sensor_f_pin, pen_axis_resolution):
+        Motor.__init__(self, name=name, step_pin=step_pin, direction_pin=direction_pin,
+                       edge_sensor_i_pin=edge_sensor_i_pin, edge_sensor_f_pin=edge_sensor_f_pin,
+                       resolution=pen_axis_resolution)
+
+    def lift_pen(self):
+        self.motor_for_loop(velocity=100, distance=50, direction='up')
+
+    def draw(self, intensity):
+        self.motor_while_loop(velocity=100, direction='down')
+        self.motor_for_loop(velocity=100, distance=intensity, direction='down')
+
 
 class Gantry:
     def __init__(self):
-        linear_axis_resolution = 8 * math.tan(math.radians(360 / 200))
+        linear_axis_resolution = 1  # float(4 * 8 * math.tan(math.radians(360 / 200)))
         z_axis_resolution = 1
         self.x = Motor(name='x', step_pin=37, direction_pin=35,
                        edge_sensor_i_pin=8, edge_sensor_f_pin=10,
@@ -108,31 +131,34 @@ class Gantry:
         self.y = Motor(name='y', step_pin=36, direction_pin=32,
                        edge_sensor_i_pin=12, edge_sensor_f_pin=11,
                        resolution=linear_axis_resolution)
-        self.z = Motor(name='z', step_pin=40, direction_pin=38,
-                       edge_sensor_i_pin=15, edge_sensor_f_pin=13,
-                       resolution=z_axis_resolution)
+        self.pen = PenAxis(name='z', step_pin=40, direction_pin=38,
+                           edge_sensor_i_pin=15, edge_sensor_f_pin=13,
+                           pen_axis_resolution=z_axis_resolution)
         self.x_position = self.x.position
         self.y_position = self.y.position
-        self.z_position = self.z.position
+        self.pen_position = self.pen.position
 
-    def homing_sequence(self):
+    def homing_sequence(self, padding):
         x_thread = threading.Thread(target=self.x.motor_while_loop, args=(400, 'backward',))
-        y_thread = threading.Thread(target=self.y.motor_while_loop, args=(400, 'right',))
-        z_thread = threading.Thread(target=self.z.motor_while_loop, args=(50, 'up',))
+        y_thread = threading.Thread(target=self.y.motor_while_loop, args=(400, 'left',))
+        self.pen.motor_while_loop(100, 'up')
 
         x_thread.start()
         y_thread.start()
-        z_thread.start()
 
         x_thread.join()
         y_thread.join()
-        z_thread.join()
 
-        self.x_position = 0
-        self.y_position = 0
-        self.z_position = 0
+        self.x.position = 0
+        self.y.position = 0
+        self.pen.position = 0
 
-        print('Pen is above (0, 0).')
+        self.x.motor_for_loop(100, padding, 'forward')
+        self.y.motor_for_loop(100, padding, 'right')
+
+        self.update_gantry_position()
+
+        print(f'Pen is above ({self.x.position}, {self.y.position}).')
 
     def calculate_steps(self, axis_name, next_position):
         if axis_name == 'x':
@@ -146,8 +172,8 @@ class Gantry:
             next_position = next_position['y']
 
         if axis_name == 'z':
-            current_position = self.z_position
-            resolution = self.z.resolution
+            current_position = self.pen_position
+            resolution = self.pen.resolution
             next_position = next_position['z']
 
         return int(abs(current_position - next_position) / resolution)
@@ -162,58 +188,69 @@ class Gantry:
     def gantry_for_loop(self, velocity, direction, steps, axis):
         velocity = 1 / velocity
         GPIO.output(axis.direction_pin, direction)
-        for step in tqdm(range(steps)):
+        for step in range(steps):
             axis.update_edge_sensors()
-            # if (axis.direction_key[direction] and not axis.edge_sensor_i_pin_state) or (
-            #         not axis.direction_key[direction] and not axis.edge_sensor_f_pin_state):
-            axis.motor_single_step(velocity)
-            axis.update_position(direction)
-            if axis.name == 'x':
-                self.x_position = axis.position
-            if axis.name == 'y':
-                self.y_position = axis.position
-            if axis.name == 'z':
-                self.z_position = axis.position
-            # print(axis.edge_sensor_i_pin_state, axis.edge_sensor_f_pin_state)
+            if (direction == 0 and axis.edge_sensor_i_pin_state) or (
+                    direction == 1 and axis.edge_sensor_f_pin_state):
+                axis.motor_single_step(velocity)
+                axis.update_position(direction)
+                if axis.name == 'x':
+                    self.x_position = axis.position
+                if axis.name == 'y':
+                    self.y_position = axis.position
+                if axis.name == 'z':
+                    self.pen_position = axis.position
+                # print(axis.edge_sensor_i_pin_state, axis.edge_sensor_f_pin_state)
 
         return f'{axis.name} axis done.'
 
-    def move_to_position(self, next_position, velocity):
+    def path_sequence(self, next_position, velocity):
         x_steps_amount = self.calculate_steps('x', next_position)
         x_direction = self.calculate_direction(self.x_position, next_position['x'])
 
         y_steps_amount = self.calculate_steps('y', next_position)
         y_direction = self.calculate_direction(self.y_position, next_position['y'])
 
-        z_steps_amount = self.calculate_steps('z', next_position)
-        z_direction = self.calculate_direction(self.z_position, next_position['z'])
-
         x_thread = threading.Thread(target=self.gantry_for_loop,
                                     args=(velocity['x'], x_direction, x_steps_amount, self.x,))
         y_thread = threading.Thread(target=self.gantry_for_loop,
                                     args=(velocity['y'], y_direction, y_steps_amount, self.y,))
-        z_thread = threading.Thread(target=self.gantry_for_loop,
-                                    args=(velocity['z'], z_direction, z_steps_amount, self.z,))
 
         x_thread.start()
         y_thread.start()
-        z_thread.start()
 
         x_thread.join()
         y_thread.join()
-        z_thread.join()
+
+    def update_gantry_position(self):
+        self.x_position = self.x.position
+        self.y_position = self.y.position
+        self.pen_position = self.pen.position
 
 
 if __name__ == "__main__":
     gantry = Gantry()
     time.sleep(5)
-    gantry.homing_sequence()
+    pad = 150
+    gantry.homing_sequence(pad)
+    x_steps = 3400
+    y_steps = 2500
+    step_delta = 300
+    # X steps amount - 3539
+    # Y steps amount - 2772
+    gantry.pen.motor_for_loop(velocity=100, distance=850, direction='down')
+    while x_steps > step_delta and y_steps > step_delta:
+        gantry.y.motor_for_loop(velocity=100, distance=y_steps, direction='right')
+        gantry.x.motor_for_loop(velocity=100, distance=x_steps, direction='forward')
+        x_steps -= step_delta
+        y_steps -= step_delta
+        gantry.y.motor_for_loop(velocity=100, distance=y_steps, direction='left')
+        gantry.x.motor_for_loop(velocity=100, distance=x_steps, direction='backward')
+        x_steps -= step_delta
+        y_steps -= step_delta
 
-    gantry.x.motor_while_loop(400, 'forward')
-    print(f'X position: {gantry.x.position}')
+        gantry.update_gantry_position()
 
-    gantry.y.motor_while_loop(400, 'left')
-    print(f'Y position: {gantry.y.position}')
-
-    gantry.z.motor_while_loop(50, 'down')
-    print(f'Z position: {gantry.z.position}')
+        print(f'X position: {gantry.x.position}')
+        print(f'Y position: {gantry.y.position}')
+        print(f'Z position: {gantry.pen.position}')
--- main.py
+++ main.py
@@ -1,19 +1,58 @@
 import RPi.GPIO as GPIO
 import axis_control
-import image_processing
+import json
 import time
+
+
+def open_json_file(file_path):
+    with open(file_path, 'r') as f:
+        _data = json.load(f)
+    return _data
 
 
 if __name__ == "__main__":
     time.sleep(2)
+
+    # X steps amount - 3539
+    # Y steps amount - 2772
+
     gantry = axis_control.Gantry()
+    painting_commands = open_json_file(r'/home/nk47/PycharmProjects/AutoPainter/Test 2 - paths.json')
 
-    img = image_processing.load_image(r'/home/nk47/PycharmProjects/AutoPainter/potato.jpeg')
-    resized = image_processing.resize_image(img, width=1300, height=1900)
-    black_and_white = image_processing.convert_to_bw(resized)
-    painting_instructions = image_processing.image_to_instructions(black_and_white, 5)
+    pad = 539
+    frame_x = 3539 - pad
+    frame_y = 2772 - pad
 
-    gantry.homing_sequence()
+    for i, painting_command in enumerate(painting_commands):
+        for j, step in enumerate(painting_command):
+            command = step['command']
+            data = step['data']
+
+            if command == 'meta_data':
+                meta_data = data
+                x_scale_factor = 2  # frame_x / meta_data['x_length']
+                y_scale_factor = 2  # frame_y / meta_data['y_length']
+
+            if command == 'home':
+                gantry.homing_sequence(pad)
+
+            if command == 'move':
+                data = {'x': pad + (x_scale_factor * data['x']),
+                        'y': pad + (y_scale_factor * data['y'])}
+                gantry.path_sequence(next_position=data, velocity={'x': 80, 'y': 80})
+
+            if command == 'drop':
+                gantry.pen.draw(intensity=1)
+
+            if command == 'lift':
+                gantry.pen.lift_pen()
+
+            if command == 'done':
+                print('DONE!')
+                exit(0)
+
+            print(f'Step #{i}: command #{j}  -  command: {command}, data: {data}')
+
     time.sleep(1)
 
     GPIO.cleanup()
